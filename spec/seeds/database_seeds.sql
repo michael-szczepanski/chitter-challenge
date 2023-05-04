46b050df5fe0e0a7600ddf321de926b679fcf817
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-05-04 17:16:53 +0100

    Fixed typos in seeds file

--- spec/seeds/database_seeds.sql
+++ spec/seeds/database_seeds.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS accounts (
   email text,
   password text,
   name text,
-  username text,
+  username text
 );
 
 CREATE TABLE IF NOT EXISTS peeps (
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS peeps (
   time date,
   content text,
   account_id int,
-  constraint fk_accounts foreign key(account_id) reference accounts(id) on delete cascade
+  constraint fk_accounts foreign key(account_id) references accounts(id) on delete cascade
 );
 
 INSERT INTO accounts
 (name, username, email, password) VALUES
-;
+('Mike', 'mike', 'mike@mike.com', 'mike');
 
 INSERT INTO peeps
 (time, content, account_id) VALUES
-;
+('2023-05-04', 'Mike', 1);
