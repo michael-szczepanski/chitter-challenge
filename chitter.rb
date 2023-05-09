acd0bb9d3301d02d06aa36e6e98f13f7f70fa275
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-05-09 16:16:29 +0100

    Render testing.

--- chitter.rb
+++ chitter.rb
@@ -28,7 +28,7 @@ class Chitter < Sinatra::Base
   get '/' do
     peep_repo = PeepRepository.new
     account_repo = AccountRepository.new
-    session[:user] = AccountRepository.new.find_by_id(1) if session[:user].nil?
+    session[:user] = AccountRepository.new.find_by_id(2) if session[:user].nil?
     @peeps = peep_repo.list_peeps
     @accounts = account_repo.read_id_user_pairs
     @user = session[:user]
